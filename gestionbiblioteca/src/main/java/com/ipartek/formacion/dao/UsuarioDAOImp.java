5ad264b981476e37e69cbf3a07bc92d1e007a84a
erasmo <erasmo.seebold@gmail.com>
2016-09-20 09:16:48 +0200

    UsuarioDAOImp actualizado, getall getbyid create update y delete codificados, asi como la base de datos ahora tiene el procedimiento createUsuario.

--- gestionbiblioteca/src/main/java/com/ipartek/formacion/dao/UsuarioDAOImp.java
+++ gestionbiblioteca/src/main/java/com/ipartek/formacion/dao/UsuarioDAOImp.java
@@ -1,15 +1,20 @@
 package com.ipartek.formacion.dao;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.neo4j.cypher.internal.compiler.v2_1.perty.printToString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 
@@ -25,7 +30,8 @@ import com.ipartek.formacion.dao.persistence.Usuario;
 @Repository("alumnoDAOImp")
 public class UsuarioDAOImp implements UsuarioDAO {
 
-	private static final Logger logger = LoggerFactory.getLogger(UsuarioDAOImp.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(UsuarioDAOImp.class);
 
 	@Autowired
 	private DataSource dataSource;
@@ -33,8 +39,9 @@ public class UsuarioDAOImp implements UsuarioDAO {
 	private SimpleJdbcCall jdbcCall;
 
 	public void setDataSource(DataSource dataSource) {
-		// TODO Auto-generated method stub
-
+		this.dataSource = dataSource;
+		this.jdbctemplate = new JdbcTemplate(dataSource);
+		this.jdbcCall = new SimpleJdbcCall(dataSource);
 	}
 
 	public List<Usuario> getAll() {
@@ -47,27 +54,65 @@ public class UsuarioDAOImp implements UsuarioDAO {
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 		}
+		logger.info("GetAll ejecutado");
 		return usuarios;
 	}
 
 	public Usuario GetById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Usuario usuario = null;
+		final String SQL = "SELECT codigo, nombre, apellidos, fNacimiento, email FROM usuario WHERE codigo = ?;";
+		try {
+			usuario = jdbctemplate.queryForObject(SQL, new Object[] { id },
+					new UsuarioMapper());
+		} catch (EmptyResultDataAccessException e) {
+			usuario = new Usuario();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		logger.info("GetById ejecutado");
+		return usuario;
 	}
 
 	public Usuario create(Usuario usuario) {
-		// TODO Auto-generated method stub
-		return null;
+		/*
+		 * createUsuario --> Nombre del procedimiento almacenado
+		 */
+		jdbcCall.withProcedureName("createUsuario");
+		/*
+		 * SqlParameterSource (tipo Map) guarda los paramentros necesarios para
+		 * el procedimiento
+		 */
+		SqlParameterSource in = new MapSqlParameterSource()
+				.addValue("nombre", usuario.getNombre())
+				.addValue("apellidos", usuario.getApellidos())
+				.addValue("fecha", new Date(usuario.getfNacimiento().getTime()))
+				.addValue("email", usuario.getEmail());
+
+		Map<String, Object> out = jdbcCall.execute(in);
+		/*
+		 * Recogemos el parametro OUT del procedimiento
+		 */
+		usuario.setCodigo((Integer) out.get("codigo"));
+		logger.info("create ejecutado");
+		return usuario;
 	}
 
 	public Usuario update(Usuario usuario) {
-		// TODO Auto-generated method stub
-		return null;
+		final String SQL = "UPDATE usuario SET nombre = ?, apellidos = ?, fNacimiento = ?, email = ?  WHERE codigo = ?;";
+		jdbctemplate.update(
+				SQL,
+				new Object[] { usuario.getNombre(), usuario.getApellidos(),
+						usuario.getfNacimiento(), usuario.getEmail(),
+						usuario.getCodigo() });
+		logger.info("update ejecutado");
+		return usuario;
+
 	}
 
 	public void delete(int id) {
-		// TODO Auto-generated method stub
-
+		final String SQL = "DELETE FROM usuario WHERE codigo = ?;";
+		jdbctemplate.update(SQL, new Object[] { id });
+		logger.info("delete ejecutado");
 	}
 
 }
